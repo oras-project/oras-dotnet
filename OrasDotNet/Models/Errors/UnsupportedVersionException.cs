85ce123d370c1a1c9aa324ea7ea82b2503727e29
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-03-24 05:39:51 +0100

    chore : created interfaces
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

---
+++ OrasDotNet/Models/Errors/UnsupportedVersionException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrasDotNet.Models.Errors
+{
+    internal class UnsupportedVersionException : Exception
+    {
+        public UnsupportedVersionException() : base("unsupported version") { }
+    }
+}
