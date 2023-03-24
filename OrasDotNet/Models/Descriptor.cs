9d8ebc5fce957c4e4791c3c582906cf61e5287ad
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-03-24 07:15:42 +0100

    chore : added the descriptoroci type
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- OrasDotNet/Models/Descriptor.cs
+++ OrasDotNet/Models/Descriptor.cs
@@ -20,5 +20,47 @@ namespace OrasDotnet.Models
 
         [JsonPropertyName("size")]
         public long Size { get; set; }
+
+        [JsonPropertyName("urls")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public List<string> URLs { get; set; }
+
+        [JsonPropertyName("annotations")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Dictionary<string, string> Annotations { get; set; }
+
+        [JsonPropertyName("data")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public byte[] Data { get; set; }
+
+        [JsonPropertyName("platform")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Platform Platform { get; set; }
+
+        [JsonPropertyName("artifactType")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string ArtifactType { get; set; }
     }
+
+    internal class Platform
+    {
+        [JsonPropertyName("architecture")]
+        public string Architecture { get; set; }
+
+        [JsonPropertyName("os")]
+        public string OS { get; set; }
+
+        [JsonPropertyName("os.version")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string OSVersion { get; set; }
+
+        [JsonPropertyName("os.features")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public List<string> OSFeatures { get; set; }
+
+        [JsonPropertyName("variant")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Variant { get; set; }
+    }
+
 }
