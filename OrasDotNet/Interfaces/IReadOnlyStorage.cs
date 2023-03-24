fe7fd6b5c04196fb5978e8432cd40d7a155c264c
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-03-24 05:39:51 +0100

    chore : created interfaces
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

---
+++ OrasDotNet/Interfaces/IReadOnlyStorage.cs
@@ -0,0 +1,15 @@
+﻿using OrasDotnet.Models;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OrasDotnet.Interfaces
+{
+    internal interface IReadOnlyStorage
+    {
+
+        Task<bool> ExistsAsync(Descriptor target, CancellationToken cancellationToken = default);
+
+        Task<Stream> FetchAsync(Descriptor target, CancellationToken cancellationToken = default);
+    }
+}
---
+++ OrasDotNet/Interfaces/ITagResolver.cs
@@ -0,0 +1,16 @@
+﻿using OrasDotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OrasDotnet.Models;
+
+namespace OrasDotnet.Interfaces
+{
+    internal interface ITagResolver
+    {
+        Task<Descriptor> ResolveAsync(string reference, CancellationToken cancellationToken = default);
+        Task TagAsync(string reference, Descriptor descriptor, CancellationToken cancellationToken = default);
+    }
+}
