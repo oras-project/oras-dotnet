ce926903160a1357749b17aed28139657f778c7e
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-03-27 09:48:49 +0100

    Added error messages to Exception Namespace
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- OrasDotNet/Exceptions/UnsupportedVersionException.cs
+++ OrasDotNet/Exceptions/UnsupportedVersionException.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OrasDotNet.Models.Errors
+namespace OrasDotNet.Exceptions
 {
     public class UnsupportedVersionException : Exception
     {
