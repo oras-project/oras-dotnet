74a40a775e71af142ca6f982493e1eb35853db29
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-04-10 00:09:04 +0100

    added document comments to ITarget
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- Oras/Interfaces/ITarget.cs
+++ Oras/Interfaces/ITarget.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Oras.Interfaces
 {
+    /// <summary>
+    /// Target is a CAS with generic tags
+    /// </summary>
     public interface ITarget : IStorage, ITagResolver
     {
     }
