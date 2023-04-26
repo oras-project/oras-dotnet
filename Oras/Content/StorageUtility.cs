ba1b896942d53ecd42efc9483a6443e0bb98930d
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-04-26 08:40:01 +0100

    changed variable name
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- Oras/Content/StorageUtility.cs
+++ Oras/Content/StorageUtility.cs
@@ -16,10 +16,10 @@ namespace Oras.Content
         static async Task<Byte[]> FetchAllAsync(IFetcher fetcher, Descriptor desc, CancellationToken cancellationToken)
         {
             var t = await fetcher.FetchAsync(desc, cancellationToken);
-            var tempByte = new byte[t.Length];
+            var tempBytes = new byte[t.Length];
             // ought to implement a readall function to handle verification of the stream
-            t.Read(tempByte, 0, (int)t.Length);
-            return tempByte;
+            t.Read(tempBytes, 0, (int)t.Length);
+            return tempBytes;
         }
         public static async Task<IList<Descriptor>> SuccessorsAsync(IFetcher fetcher, Descriptor node, CancellationToken cancellationToken)
         {
