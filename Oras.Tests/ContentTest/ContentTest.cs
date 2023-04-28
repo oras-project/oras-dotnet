3750b4f97ecf7a55fe7c9bbcc4ba24999d6a0abb
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-04-28 09:20:14 +0100

    added extra test to copy
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

---
+++ Oras.Tests/ContentTest/ContentTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Oras.Content.Content;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Oras.Tests.ContentTest
+{
+    public class ContentTest
+    {
+        /// <summary>
+        /// This method tests if the digest is calculated properly
+        /// </summary>
+        [Fact]
+        public void Content_EnsureThatTheDigestIsCalculatedProperly()
+        {
+            var helloWorldDigest = "sha256:11d4ddc357e0822968dbfd226b6e1c2aac018d076a54da4f65e1dc8180684ac3";
+            var content = Encoding.UTF8.GetBytes("helloWorld");
+            var calculateHelloWorldDigest = CalculateDigest(content);
+            Assert.Equal(helloWorldDigest,calculateHelloWorldDigest);
+        }
+    }
+}
