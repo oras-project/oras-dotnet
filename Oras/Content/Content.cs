54f2c335be42915871ddc40bd26a9993f16f6caf
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-04-28 08:46:04 +0100

    made some changes
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

--- Oras/Content/Content.cs
+++ Oras/Content/Content.cs
@@ -15,6 +15,13 @@ namespace Oras.Content
 {
     public static class Content
     {
+        /// <summary>
+        /// Retrieves the successors of a node
+        /// </summary>
+        /// <param name="fetcher"></param>
+        /// <param name="node"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<IList<Descriptor>> SuccessorsAsync(IFetcher fetcher, Descriptor node, CancellationToken cancellationToken)
         {
             switch (node.MediaType)
@@ -40,12 +47,26 @@ namespace Oras.Content
             return default;
         }
 
+        /// <summary>
+        /// Fetches all the content for a given descriptor.
+        /// Currently only sha256 is supported but we would supports others hash algorithms in the future.
+        /// </summary>
+        /// <param name="fetcher"></param>
+        /// <param name="desc"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<Byte[]> FetchAllAsync(IFetcher fetcher, Descriptor desc, CancellationToken cancellationToken)
         {
             var stream = await fetcher.FetchAsync(desc, cancellationToken);
             return await ReadAllAsync(stream, desc);
         }
 
+        /// <summary>
+        /// Calculates the digest of the content
+        /// Currently only sha256 is supported but we would supports others hash algorithms in the future.
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
         internal static string CalculateDigest(byte[] content)
         {
             using var sha256 = SHA256.Create();
@@ -54,7 +75,16 @@ namespace Oras.Content
             return output;
         }
 
-        public static async Task<byte[]> ReadAllAsync(Stream stream, Descriptor descriptor)
+        /// <summary>
+        /// Reads and verifies the content from a stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="descriptor"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidDescriptorSizeException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="MismatchedDigestException"></exception>
+        internal static async Task<byte[]> ReadAllAsync(Stream stream, Descriptor descriptor)
         {
             if (descriptor.Size < 0)
             {
