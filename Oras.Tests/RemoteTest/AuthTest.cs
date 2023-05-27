e67ded29a58dcc860a4dfd6e75878b734a5ba45f
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-05-27 17:18:16 +0100

    feat: added support for basic auth (#23)
    fixes #19
    
    Signed-off-by: Samson Amaugo <sammychinedu2ky@gmail.com>

---
+++ Oras.Tests/RemoteTest/AuthTest.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using Moq.Protected;
+using Oras.Remote;
+using Oras.Remote.Auth;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace Oras.Tests.RemoteTest
+{
+    public class AuthTest
+    {
+
+        public static HttpClient CustomClient(Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> func, string username, string password)
+        {
+            var moqHandler = new Mock<DelegatingHandler>();
+            moqHandler.Protected().Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            ).ReturnsAsync(func);
+            return new HttpClientWithBasicAuth(username, password, moqHandler.Object);
+        }
+
+        /// <summary>
+        /// TestClient_CustomHttpBasicAuthClient tests the CustomHttpBasicAuthClient class.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task TestClient_CustomHttpBasicAuthClient()
+        {
+            var username = "test_user";
+            var password = "test_password";
+            var func = (HttpRequestMessage req, CancellationToken cancellationToken) =>
+            {
+                var res = new HttpResponseMessage();
+                res.RequestMessage = req;
+                if (req.Method != HttpMethod.Get && req.RequestUri.AbsolutePath == $"/")
+                {
+                    res.StatusCode = HttpStatusCode.NotFound;
+                    return res;
+                }
+                var authHeader = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+                if (req.Headers.Authorization.ToString() != authHeader)
+                {
+                    res.Headers.Add("WWW-Authenticate", "Basic realm=\"test\"");
+                    res.StatusCode = HttpStatusCode.Unauthorized;
+                    return res;
+                }
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            };
+            var client = CustomClient(func, username, password);
+            var response = await client.GetAsync("http://localhost:5000");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
