19fcd476e544b91add5adf4449c2af46318b82e5
Shiwei Zhang <shizh@microsoft.com>
2024-01-04 21:47:16 +0800

    refactor: modernize HttpClientWithBasicAuth
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- src/OrasProject.Oras/Registry/Remote/Auth/HttpClientWithBasicAuth.cs
+++ src/OrasProject.Oras/Registry/Remote/Auth/HttpClientWithBasicAuth.cs
@@ -11,32 +11,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using OrasProject.Oras.Registry.Remote;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 
-namespace OrasProject.Oras.Remote.Auth
+namespace OrasProject.Oras.Registry.Remote.Auth;
+
+/// <summary>
+/// HttpClientWithBasicAuth adds the Basic Auth Scheme to the Authorization Header
+/// </summary>
+public class HttpClientWithBasicAuth : HttpClient
 {
-    /// <summary>
-    /// HttpClientWithBasicAuth adds the Basic Auth Scheme to the Authorization Header
-    /// </summary>
-    public class HttpClientWithBasicAuth : HttpClient
+    public HttpClientWithBasicAuth(string username, string password) => Initialize(username, password);
+
+    public HttpClientWithBasicAuth(string username, string password, HttpMessageHandler handler) : base(handler)
+        => Initialize(username, password);
+
+    private void Initialize(string username, string password)
     {
-        public HttpClientWithBasicAuth(string username, string password)
-        {
-            this.AddUserAgent();
-            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
-        }
-
-        public HttpClientWithBasicAuth(string username, string password, HttpMessageHandler handler) : base(handler)
-        {
-            this.AddUserAgent();
-            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
-        }
-
+        this.AddUserAgent();
+        DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+            Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
     }
 }
