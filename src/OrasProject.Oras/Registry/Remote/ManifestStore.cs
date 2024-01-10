c53d9c1d3dc75f9b5123d8ac61684a5f16eed1f9
Shiwei Zhang <shizh@microsoft.com>
2024-01-10 14:33:04 +0800

    chore: use HttpIOException instead of generic Exception
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- src/OrasProject.Oras/Registry/Remote/ManifestStore.cs
+++ src/OrasProject.Oras/Registry/Remote/ManifestStore.cs
@@ -55,11 +55,12 @@ public class ManifestStore(Repository repository) : IManifestStore
             var mediaType = response.Content.Headers.ContentType?.MediaType;
             if (mediaType != target.MediaType)
             {
-                throw new Exception($"{response.RequestMessage!.Method} {response.RequestMessage.RequestUri}: mismatch response Content-Type {mediaType}: expect {target.MediaType}");
+                throw new HttpIOException(HttpRequestError.InvalidResponse, $"{response.RequestMessage!.Method} {response.RequestMessage.RequestUri}: mismatch response Content-Type {mediaType}: expect {target.MediaType}");
             }
-            if (response.Content.Headers.ContentLength is var size && size != -1 && size != target.Size)
+            var size = response.Content.Headers.ContentLength;
+            if (size != null && size != target.Size)
             {
-                throw new Exception($"{response.RequestMessage!.Method} {response.RequestMessage.RequestUri}: mismatch Content-Length");
+                throw new HttpIOException(HttpRequestError.InvalidResponse, $"{response.RequestMessage!.Method} {response.RequestMessage.RequestUri}: mismatch Content-Length");
             }
             response.VerifyContentDigest(target.Digest);
             return await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
@@ -90,7 +91,7 @@ public class ManifestStore(Repository repository) : IManifestStore
             {
                 case HttpStatusCode.OK:
                     Descriptor desc;
-                    if (response.Content.Headers.ContentLength == -1)
+                    if (response.Content.Headers.ContentLength == null)
                     {
                         desc = await ResolveAsync(reference, cancellationToken).ConfigureAwait(false);
                     }
