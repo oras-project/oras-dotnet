6122cc38d7d16d41d2e4f0ba5010ce5174c53941
Xiaoxuan Wang <wangxiaoxuan119@gmail.com>
2025-09-09 14:10:47 +0800

    revert change in copy options
    Signed-off-by: Xiaoxuan Wang <wangxiaoxuan119@gmail.com>

--- src/OrasProject.Oras/CopyOptions.cs
+++ src/OrasProject.Oras/CopyOptions.cs
@@ -11,13 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using OrasProject.Oras.Content;
-using OrasProject.Oras.Oci;
-
 namespace OrasProject.Oras;
 
 public class CopyOptions : CopyGraphOptions { }
